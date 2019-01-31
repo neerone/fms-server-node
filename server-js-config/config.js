24b54f87a020ec0f1682b18f572a1be4946da7d3
Tony Plato <neuroweet@gmail.com>
2019-01-31 16:39:34 +0300

    add config

---
+++ server-js-config/config.js
@@ -0,0 +1,11 @@
+let mysqlconnection = {
+    host     : 'localhost',
+    user     : 'root',
+    password : 'sa3Keeth',
+    database : 'simma'
+  }
+
+var uploadspath = "/home/estaco.ru/priv/static/uploads/";
+//var uploadspath = "G:\\work\\elm-sys2\\client\\src\\static\\uploads\\";
+
+module.exports = {mysqlconnection,uploadspath};
